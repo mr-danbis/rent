ef6ddbeea0c746007f7a992db6ca2e0f6dcbd902
Daniel <dan.bisyukov@gmail.com>
2022-11-03 21:17:55 +0300

    Added modal

--- app/js/main.js
+++ app/js/main.js
@@ -39,47 +39,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-    // class Tabs {
-    //     constructor(navList, navItem, itemActiveClass, contentDBClasses, activeContentClass) {
-    //         this.navList = navList;
-    //         this.navItem = navItem;
-    //         this.itemActiveClass = itemActiveClass;
-    //         this.contentDBClasses = contentDBClasses;
-    //         this.activeContentClass = activeContentClass;
-    //     }
-
-    //     addingActiveClass(navList, navItem, itemActiveClass, contentDBClasses, activeContentClass) {
-
-    //         document.querySelector(navList).addEventListener('click', (e) => {
-
-    //             if (e.target && !e.target.classList.contains(itemActiveClass)) {
-    //                 e.target.classList.add(itemActiveClass);
-    //             } else {
-    //                 return;
-    //             }
-
-    //             document.querySelectorAll(navItem).forEach(item => {
-    //                 if (item.classList.contains(itemActiveClass) && item != e.target) {
-    //                     item.classList.remove(itemActiveClass);
-    //                 }
-    //             });
-
-    //             for (let i = 0; i < contentDBClasses.length; i++) {
-    //                 if (e.target.id && e.target.id.slice(-1) == i + 1) {
-    //                     document.querySelector(contentDBClasses[i]).classList.add(activeContentClass);
-    //                 } else {
-    //                     document.querySelector(contentDBClasses[i]).classList.remove(activeContentClass);
-    //                 }
-    //             }
-    //         });
-    //     }
-    // }
-
-    // const accountMainNavigation = new Tabs();
-    // const contentDBClasses = ['.account-login', '.account-data', 'account-my', '.account-favourites'];
-    // accountMainNavigation.addingActiveClass('.account-navigation__list', '.account-navigation__item', 'account-navigation__item--active', 'account-content__inner-visible', contentDBClasses);
-
-
 
     function accountMainTabs() {
         const accountNavItem = $(".account-navigation__tabs");
@@ -111,6 +70,22 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
     accountNewTabs();
+
+
+    function openModal(){
+        const modal = document.querySelector('.modal');
+        const changePasswordBtn = document.querySelector('.form__change-password');
+        const modalCloseBtn = document.querySelector('.modal__btn-close');
+
+        changePasswordBtn.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
+        modalCloseBtn.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+
+    }
+    openModal();
 
 
 });
